f82e3cd871c107ff0c5c11f51f12e170da6b6926
jayjay2626 <rangeleydelphin826@yahoo.com>
2021-02-17 04:25:39 -0500

    updated the default.rb recipe file

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,14 +4,6 @@
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
 
-
-python_installation 'No Defaults' do
-  version '3.6.9'
-  download_directory '/usr/local/python-dl'
-  build_directory '/usr/local/python-bld'
-  install_directory '/usr/local/python'
-  openssl_directory '/opt/openssl/1.1.1c'
-  sqlite_directory '/opt/sqlite/3300000'
-  owner 'jayjay'
-  group 'jayjay'
+package "python3" do
+  action :install
 end
